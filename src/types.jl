91c7e8cc8d567af4b2baaa406b848fd3c20c60d5
kalmarek <kalmar@amu.edu.pl>
2017-02-14 02:10:25 +0100

    update status_mapt to latest constants
    Does `SCS_SOLVED_INACCURATE` qualify for `:UserLimit`?

--- src/types.jl
+++ src/types.jl
@@ -80,13 +80,16 @@ immutable SCSCone
 end
 
 
-# TODO needs to be updated for newest constants
 const status_map = Dict{Int, Symbol}(
+    2 => :UserLimit,
     1 => :Optimal,
-    -2 => :Infeasible,
     -1 => :Unbounded,
+    -2 => :Infeasible,
     -3 => :Indeterminate,
-    -4 => :Error
+    -4 => :Error,
+    -5 => :Interrupted,
+    -6 => :UnboundedInaccurate,
+    -7 => :InfeasibleInaccurate
 )
 
 type Solution
