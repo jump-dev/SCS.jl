acea62b72c78a758e7b78fad13ccb87f6e326c0e
kalmar <kalmar@amu.edu.pl>
2017-03-04 16:38:22 +0100

    SCS_init/solve/finish without type fallback to Direct

--- src/low_level_wrapper.jl
+++ src/low_level_wrapper.jl
@@ -51,37 +51,20 @@ for (T, lib) in zip([SCS.Direct, SCS.Indirect], [SCS.scs, SCS.scsindir])
     end
 end
 
+SCS_init(data::SCSData, cone::SCSCone) = SCS_init(SCS.Direct, data, cone)
 
-function SCS_solve(p_work::Ptr{Void}, data::SCSData, cone::SCSCone, info::SCSInfo, solution::SCSSolution)
-    solution_ptr = pointer([solution])
-
-    info_ptr = pointer([info])
-
-    status = ccall((:scs_solve, SCS.scs), Int,
-        (Ptr{Void}, Ptr{SCSData}, Ptr{SCSCone}, Ptr{SCSSolution}, Ptr{SCSInfo}),
-        p_work, &data, &cone, solution_ptr, info_ptr)
-
-    solution = unsafe_load(solution_ptr)
-    info = unsafe_load(info_ptr)
-
-    return status, solution, info, p_work
+function SCS_solve(p_work::Ptr{Void}, data::SCSData, cone::SCSCone,
+        info::SCSInfo, solution::SCSSolution)
+    return SCS_solve(SCS.Direct, p_work, data, cone, info, solution)
 end
 
-
-function SCS_solve(p_work::Ptr{Void}, data::SCSData, cone::SCSCone, info::SCSInfo)
-    solution = SCSSolution(pointer(zeros(data.n)), pointer(zeros(data.m)), pointer(zeros(data.m)))
-    return SCS_solve(p_work, data, cone, info, solution)
+function SCS_solve(p_work::Ptr{Void}, data::SCSData, cone::SCSCone,
+        info::SCSInfo)
+    return SCS_solve(SCS.Direct, p_work, data, cone, info)
 end
-
 
 function SCS_solve(data::SCSData, cone::SCSCone, solution::SCSSolution)
-    p_work, info = SCS_init(data, cone)
-    return SCS_solve(p_work, data, cone, info, solution)
+    return SCS_solve(SCS.Direct, data, cone, solution)
 end
 
-
-function SCS_finish(p_work::Ptr{Void})
-    ccall((:scs_finish, SCS.scs), Void,
-        (Ptr{Void}, ),
-        p_work)
-end
+SCS_finish(p_work::Ptr{Void}) = SCS_finish(SCS.Direct, p_work)
