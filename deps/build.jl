7b9684f403be9252f18cf4919cd8cedc0b6afd62
Miles Lubin <mlubin@google.com>
2017-11-29 22:23:07 -0500

    comment about windows support

--- deps/build.jl
+++ deps/build.jl
@@ -19,7 +19,7 @@ if is_apple()
 end
 
 version = "2.0.0"
-win_version = "2.0.0"
+win_version = "2.0.0" # 2.0.0 is not yet working on windows
 
 provides(Sources, URI("https://github.com/cvxgrp/scs/archive/v$version.tar.gz"),
     [scs], os=:Unix, unpacked_dir="scs-$version")
