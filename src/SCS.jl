ca080afb44d8e8826ea8c66767a308646a4af133
Benoît Legat <benoit.legat@gmail.com>
2019-05-27 08:41:55 +0200

    Update deps.jl loading

--- src/SCS.jl
+++ src/SCS.jl
@@ -1,20 +1,14 @@
 module SCS
 
-# To make Pkg aware that this dependency
-# will be injected by BinaryProvider. 		
-if VERSION > v"0.7.0-"
-    using Libdl
-end
-
 if isfile(joinpath(dirname(@__FILE__), "..", "deps", "deps.jl"))
-    include("../deps/deps.jl")
+    include(joinpath(dirname(@__FILE__), "..", "deps", "deps.jl"))
 else
     error("SCS not properly installed. Please run Pkg.build(\"SCS\") and restart julia")
 end
 
 function __init__()
     vnum = VersionNumber(SCS_version())
-    depsdir = realpath(joinpath(dirname(@__FILE__),"..","deps"))
+    depsdir = realpath(joinpath(dirname(@__FILE__), "..", "deps"))
     if vnum.major == 1
         error("Current SCS version installed is $(SCS_version()), but we require version 2.*.")
     end
