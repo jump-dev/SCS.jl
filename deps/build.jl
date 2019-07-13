3261eae03acdcca0016516979cf4a2bf60066564
Juan Pablo Vielma <jvielma@mit.edu>
2019-07-13 15:41:47 -0400

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -69,58 +69,8 @@ if haskey(ENV,"JULIA_SCS_LIBRARY_PATH")
     else
         error("Could not install custom libraries from $(ENV["JULIA_SCS_LIBRARY_PATH"]).\nTo fall back to BinaryProvider call delete!(ENV,\"JULIA_SCS_LIBRARY_PATH\") and run build again.")
     end
-end
-
-# The code below to deal with non-default-named BLAS libraries is taken from here
-# https://github.com/JuliaLinearAlgebra/Arpack.jl/blob/c7b6813f04a5213c53d4d88fb85d3db4a9a3d856/deps/build.jl#L60-L145
-# These portions are marked with *START_BLAS* / *END_BLAS* and can be removed without affecting the rest of the build script
-
-# *START_BLAS*
-# Regenerate this with `get_default_blaslib_names.jl`
-default_blaslib_names = Dict(
-    Linux(:i686, libc=:musl, compiler_abi=CompilerABI(:gcc8)) => "libopenblas.so.0",
-    Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.so.0",
-    Windows(:i686, compiler_abi=CompilerABI(:gcc7)) => "libopenblas.dll",
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.dylib",
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.dylib",
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc8)) => "libopenblas.so.0",
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.so.0",
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc7)) => "libopenblas.so.0",
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc8)) => "libopenblas.so.0",
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.so.0",
-    FreeBSD(:x86_64, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.so",
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.so.0",
-    FreeBSD(:x86_64, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.so",
-    Windows(:x86_64, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.dll",
-    Windows(:x86_64, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.dll",
-    Windows(:i686, compiler_abi=CompilerABI(:gcc4)) => "libopenblas.dll",
-    Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.so.0",
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.dylib",
-    Linux(:i686, libc=:musl, compiler_abi=CompilerABI(:gcc7)) => "libopenblas.so.0",
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc7)) => "libopenblas.so.0",
-    Linux(:aarch64, libc=:musl, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.so.0",
-    Linux(:powerpc64le, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.so.0",
-    Linux(:i686, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => "libopenblas.so.0",
-    Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.so.0",
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc4)) => "libopenblas.so.0",
-    Linux(:i686, libc=:glibc, compiler_abi=CompilerABI(:gcc4)) => "libopenblas.so.0",
-    Windows(:i686, compiler_abi=CompilerABI(:gcc8)) => "libopenblas.dll",
-    Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.so.0",
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf, compiler_abi=CompilerABI(:gcc4)) => "libopenblas.so.0",
-    Linux(:powerpc64le, libc=:glibc, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.so.0",
-    Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.so.0",
-    FreeBSD(:x86_64, compiler_abi=CompilerABI(:gcc4)) => "libopenblas64_.so",
-    Linux(:powerpc64le, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.so.0",
-    Linux(:i686, libc=:glibc, compiler_abi=CompilerABI(:gcc8)) => "libopenblas.so.0",
-    Linux(:aarch64, libc=:musl, compiler_abi=CompilerABI(:gcc8)) => "libopenblas64_.so.0",
-    Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.so.0",
-    Windows(:x86_64, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.dll",
-    Linux(:i686, libc=:musl, compiler_abi=CompilerABI(:gcc4)) => "libopenblas.so.0",
-    Linux(:aarch64, libc=:musl, compiler_abi=CompilerABI(:gcc7)) => "libopenblas64_.so.0",
-)
-# *END_BLAS*                    
-                  
-                    
+end        
+                                   
 if !custom_library
     # Install unsatisfied or updated dependencies:
     unsatisfied = any(!satisfied(p; verbose=verbose) for p in products)
@@ -140,41 +90,6 @@ if !custom_library
         install(dl_info...; prefix=prefix, force=true, verbose=verbose)
     end
                
-    # *START_BLAS*
-                    
-    # Distribution packagers love to rename their BLAS libraries, which is naturally
-    # incompatible with precompiled binaries.  We attempt to address this by automatically
-    # creating a symlink to the currently loaded BLAS library that is named what SCS is
-    # attempting to load.
-    blaslib = first(filter(x -> occursin(Base.libblas_name, x), dllist()))
-    default_blaslib_name = choose_download(default_blaslib_names, platform_key_abi())
-    sym_file = joinpath(abspath(joinpath(prefix,"lib")), default_blaslib_name)
-    if !isfile(sym_file) && basename(blaslib) != default_blaslib_name && !isfile(joinpath(dirname(blaslib), default_blaslib_name))
-        #sym_file = joinpath(abspath(joinpath(Sys.BINDIR, Base.LIBDIR)), default_blaslib_name)
-        
-        @info(strip(replace(
-        """
-        This Julia installation uses a non-default BLAS library name (\"$(basename(blaslib))\"
-        instead of \"$(default_blaslib_name)\")!  This is likely due to using a distribution
-        package, and can cause problems when installing binary dependencies such as SCS.jl.
-        We will attempt to automatically map the distribution-provided BLAS library to such
-        that libscsindir/libscsdir can find it, but this may not work.  If issues with SCS.jl persist,
-        we recommend using the Julia binaries available for download from the official website
-        at https://julialang.org/downloads/, or to provide custom libraries by setting ENV[\"JULIA_SCS_LIBRARY_PATH\"])
-        """, '\n' => ' ')))
-
-        @warn(strip(replace(
-        """
-        Attempting to symlink \"$(sym_file)\" => \"$(blaslib)\".
-        """, '\n' => ' ')))
-#         @warn(strip(replace(
-#         """
-#         Attempting to symlink \"$(sym_file)\" => \"$(blaslib)\"; if this fails, try running it
-#         manually with super user permissions via: sudo ln -s $(blaslib) $(sym_file)
-#         """, '\n' => ' ')))
-        symlink(blaslib, sym_file)
-    end
-    # *END_BLAS* 
  end
                     
 # Write out a deps.jl file that will contain mappings for our products
