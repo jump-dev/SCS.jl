8dc928934e0dcc0305323af949f9bf78eaec62de
kalmarek <kalmar@amu.edu.pl>
2020-10-21 11:36:37 +0200

    tests: move feasible_basic_problems

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,17 +12,6 @@ solvers = SCS.available_solvers
 
 include("test_problems.jl")
 
-function feasible_basic_problems(solver)
-    A = reshape([1.0],(1,1))
-    solution = SCS_solve(solver, 1, 1, A, [1.0], [1.0], 1, 0, Int[], Int[], 0, 0, Float64[]);
-    @test solution.ret_val == 1
-
-    feasible_basic_conic(solver)
-    feasible_exponential_conic(solver)
-    feasible_sdp_conic(solver)
-    feasible_pow_conic(solver)
-end
-
 for s in solvers
     feasible_basic_problems(s)
 end
