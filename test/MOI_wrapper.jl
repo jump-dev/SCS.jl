76d74aab1b88ec713fd86ecb473e0f5938edbd3f
Oscar Dowson <odow@users.noreply.github.com>
2021-11-10 10:22:29 +1300

    Skip SolverVersion test

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -50,6 +50,8 @@ function _test_runtests(linear_solver)
             ],
         ),
         exclude = String[
+            # TODO(odow): unimplemented features
+            "test_attribute_SolverVersion",
             # Expected test failures:
             #   ArgumentError: The number of constraints must be greater than 0
             "test_attribute_RawStatusString",
