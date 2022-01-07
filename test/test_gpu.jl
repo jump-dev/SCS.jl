bebb55c5e650b07a5fdaf7d09abf28762b6b0745
odow <o.dowson@gmail.com>
2022-01-07 16:22:17 +1300

    Revert "Testing for GPU"
    This reverts commit 92035666afb557a40a1ba5bd835a9692f299548c.

--- test/test_gpu.jl
+++ test/test_gpu.jl
@@ -7,21 +7,16 @@ Pkg.add(Pkg.PackageSpec(name = "CUDA_jll", version = "10.1"))
 
 using CUDA_jll  # CUDA_jll must be loaded _before_ SCS!
 using SCS
+
 using Test
 
+@test SCS.GpuIndirectSolver in SCS.available_solvers
+
+@test @ccall(SCS.gpuindirect.scs_sizeof_int()::Csize_t) ==
+      sizeof(SCS.scsint_t(SCS.GpuIndirectSolver))
+
 include("test_problems.jl")
-
-@testset "Intro" begin
-    @test SCS.GpuIndirectSolver in SCS.available_solvers
-    @test(
-          @ccall(SCS.gpuindirect.scs_sizeof_int()::Csize_t) ==
-          sizeof(SCS.scsint_t(SCS.GpuIndirectSolver)),
-    )
-end
-
-@testset "basic_feasible_problems" begin
-    feasible_basic_problems(SCS.GpuIndirectSolver)
-end
+feasible_basic_problems(SCS.GpuIndirectSolver)
 
 # TODO(odow): consider re-enabling these
 # include("MOI_wrapper.jl")
