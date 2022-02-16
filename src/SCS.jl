acdca37ede210cb8b7a019a3797a206fc5294bee
Oscar Dowson <odow@users.noreply.github.com>
2022-02-16 21:47:15 +1300

    Set library path in __init__ (#248)

--- src/SCS.jl
+++ src/SCS.jl
@@ -4,15 +4,14 @@ import Requires
 import SCS_jll
 import SparseArrays
 
-const indirect = SCS_jll.libscsindir
-const direct = SCS_jll.libscsdir
-
 function __init__()
+    global indirect = SCS_jll.libscsindir
+    global direct = SCS_jll.libscsdir
     Requires.@require(
         CUDA_jll = "e9e359dc-d701-5aa8-82ae-09bbf812ea83",
         begin
             import SCS_GPU_jll
-            const gpuindirect = SCS_GPU_jll.libscsgpuindir
+            global gpuindirect = SCS_GPU_jll.libscsgpuindir
             push!(available_solvers, GpuIndirectSolver)
         end
     )
