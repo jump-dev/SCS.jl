5caff49917c04755a0541de0518b6ad3e58174bb
Marek Kaluba <kalmar@amu.edu.pl>
2022-04-18 21:25:38 +0200

    hide MKLDirectSolver behind @require

--- src/SCS.jl
+++ src/SCS.jl
@@ -3,24 +3,11 @@ module SCS
 import MathOptInterface
 import Requires
 import SCS_jll
-import SCS_MKL_jll
-import SCS_MKL_jll.MKL_jll
-import Libdl
 import SparseArrays
 
 function __init__()
     global indirect = SCS_jll.libscsindir
     global direct = SCS_jll.libscsdir
-    global mkldirect = SCS_MKL_jll.libscsmkl
-
-    MKLROOT = joinpath(MKL_jll.artifact_dir, "lib")
-    # dlopen with RTLD_GLOBAL to avoid undefined symbols when dlopening
-    # `lib_mkl_gnu_thread.so`
-    Libdl.dlopen(
-        joinpath(MKLROOT, "libmkl_core.so"),
-        Libdl.RTLD_LAZY | Libdl.RTLD_DEEPBIND | Libdl.RTLD_GLOBAL,
-    )
-    Libdl.dlopen(joinpath(MKLROOT, "libmkl_gnu_thread.so"))
 
     Requires.@require(
         CUDA_jll = "e9e359dc-d701-5aa8-82ae-09bbf812ea83",
@@ -28,6 +15,26 @@ function __init__()
             import SCS_GPU_jll
             global gpuindirect = SCS_GPU_jll.libscsgpuindir
             push!(available_solvers, GpuIndirectSolver)
+        end
+    )
+    Requires.@require(
+        MKL_jll = "856f044c-d86e-5d09-b602-aeab76dc8ba7",
+        begin
+            import SCS_MKL_jll
+            import SCS_MKL_jll.MKL_jll
+            import Libdl
+            global mkldirect = SCS_MKL_jll.libscsmkl
+
+            MKLROOT = joinpath(MKL_jll.artifact_dir, "lib")
+            # dlopen with RTLD_GLOBAL to avoid undefined symbols when dlopening
+            # `lib_mkl_gnu_thread.so`
+            Libdl.dlopen(
+                joinpath(MKLROOT, "libmkl_core.so"),
+                Libdl.RTLD_LAZY | Libdl.RTLD_DEEPBIND | Libdl.RTLD_GLOBAL,
+            )
+            Libdl.dlopen(joinpath(MKLROOT, "libmkl_gnu_thread.so"))
+
+            push!(available_solvers, MKLDirectSolver)
         end
     )
     return
@@ -40,7 +47,7 @@ include("linear_solvers/gpu_indirect.jl")
 include("linear_solvers/mkl_direct.jl")
 include("MOI_wrapper/MOI_wrapper.jl")
 
-const available_solvers = [DirectSolver, MKLDirectSolver, IndirectSolver]
+const available_solvers = [DirectSolver, IndirectSolver]
 
 export scs_solve
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,17 @@ if get(ENV, "BUILDKITE", "false") == "true"
 end
 
 using Test
+# MKL_jll is not in our Project.toml, so we need to install it.
+import Pkg
+Pkg.add(Pkg.PackageSpec(name = "MKL_jll", version = "2022"))
+using MKL_jll  # MKL_jll must be loaded _before_ SCS!
 using SCS
+
+@test SCS.MKLDirectSolver in SCS.available_solvers
 
 include("test_problems.jl")
 @test SCS.scs_version() isa String
-@test VersionNumber() >= v"3.2.0"
+@test VersionNumber(SCS.scs_version()) >= v"3.2.0"
 for solver in SCS.available_solvers
     @test SCS.scs_version(solver) isa String
     @test VersionNumber(SCS.scs_version(solver)) >= v"3.2.0"
