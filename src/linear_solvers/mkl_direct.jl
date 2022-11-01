53427ca53614447484f5e8456d9b749ae84aaa8f
Marek Kaluba <kalmar@amu.edu.pl>
2022-11-02 00:56:00 +0100

    add copyright notice to mkl_direct.jl

--- src/linear_solvers/mkl_direct.jl
+++ src/linear_solvers/mkl_direct.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2022: SCS.jl contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 struct MKLDirectSolver <: LinearSolver end
 
 scsint_t(::Type{MKLDirectSolver}) = Clonglong
