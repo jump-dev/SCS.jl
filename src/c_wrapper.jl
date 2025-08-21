jump-dev/SCS.jl

commit 9289a31ef6da5b2c2d09217b3928ce8169c46e7d

Add support for SCS@3.2.8 and the complex psd cone (#311)

src/c_wrapper.jl
@@ -56,6 +56,8 @@ mutable struct ScsCone{T} <: AbstractSCSType
     qsize::T
     s::Ptr{T}
     ssize::T
+    cs::Ptr{T}
+    cssize::T
     ep::T
     ed::T
     p::Ptr{Cdouble}
@@ -130,6 +132,7 @@ struct _ScsDataWrapper{S,T}
     bl::Vector{Cdouble}
     q::Vector{T}
     s::Vector{T}
+    cs::Vector{T}
     ep::T
     ed::T
     p::Vector{Cdouble}
@@ -195,7 +198,8 @@ where K is a product cone of
 - positive orthant `{ x | x ≥ 0 }`
 - box cone `{ (t,x) | t*l ≤ x ≤ t*u }`
 - second-order cone (SOC) `{ (t,x) | ||x||_2 ≤ t }`
-- semi-definite cone (SDC) `{ X | X is psd }`
+- positive semi-definite (PSD) cone `{ X ∈ ℝⁿˣⁿ | X is psd }`
+- complex positive semi-definite cone `{ X ∈ ℂⁿˣⁿ | X is psd }`
 - exponential cone `{ (x,y,z) | y e^(x/y) ≤ z, y > 0 }`
 - power cone `{ (x,y,z) | x^a * y^(1-a) ≥ |z|, x ≥ 0, y ≥ 0 }`
 - dual power cone `{ (u,v,w) | (u/a)^a * (v/(1-a))^(1-a) ≥ |w|, u ≥ 0, v ≥ 0 }`
@@ -221,7 +225,8 @@ above by the following arguments.
 - `bu`: the `Vector` of upper bounds for the box cone
 - `bl`: the `Vector` of lower bounds for the box cone
 - `q`: the `Vector` of SOCs sizes
-- `s`: the `Vector` of SDCs sizes
+- `s`: the `Vector` of PSD cones sizes
+- `cs`: the `Vector` of complex PSD cones sizes
 - `ep`: the number of primal exponential cones
 - `ed`: the number of dual exponential cones
 - `p`: the `Vector` of power cone parameters (±1, with negative values for the
@@ -257,7 +262,7 @@ optimal value of the dual variable, `s` is the slack variable, and `info`
 contains various information about the solve step.
 
 !!! warning
-    SCS expects the semi-definite cones to be scaled by a factor of √2. That is,
+    SCS expects the PSD cones to be scaled by a factor of √2. That is,
     the off-diagonal elements in the A matrix and b vector should be multiplied
     by √2, and then the corresponding rows in the dual and slack solution
     vectors should be multiplied by 1/√2.
@@ -281,6 +286,7 @@ function scs_solve(
     bl::Vector{Float64},
     q::Vector{<:Integer},
     s::Vector{<:Integer},
+    cs::Vector{<:Integer},
     ep::Integer,
     ed::Integer,
     p::Vector{Float64},
@@ -328,6 +334,7 @@ function scs_solve(
         bl,
         convert(Vector{T}, q),
         convert(Vector{T}, s),
+        convert(Vector{T}, cs),
         ep,
         ed,
         p,
@@ -353,6 +360,8 @@ function _unsafe_scs_solve(model::_ScsDataWrapper{S,T}) where {S,T}
         length(model.q),
         pointer(model.s),
         length(model.s),
+        pointer(model.cs),
+        length(model.cs),
         model.ep,
         model.ed,
         pointer(model.p),

test/test_problems.jl
@@ -22,6 +22,7 @@ function feasible_basic_conic(T)
     ed = 0
     q = [12]
     s = Int[]
+    cs = Int[]
 
     b = [
     1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
@@ -440,7 +441,7 @@ function feasible_basic_conic(T)
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
 
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, Float64[])
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, Float64[])
     @test sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
 end
@@ -458,6 +459,7 @@ function feasible_exponential_conic(T)
     ed = 5
     q = [0,1,0,2,3]
     s = Int[]
+    cs = Int[]
 
     b = [
     -0.883995908378185202,  0.247092008779755457,  0.000000000000000000,
@@ -567,12 +569,12 @@ function feasible_exponential_conic(T)
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
 
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, Float64[])
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, Float64[])
     @assert sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
 end
 
-# Feasible conic problem with exponential and SDP cones
+# Feasible conic problem with exponential and real SDP cones
 # BLAS and LAPACK must be linked
 # Problem data taken from https://github.com/cvxgrp/scs/blob/master/examples/raw/randomConeFeasibleSDP
 function feasible_sdp_conic(T)
@@ -586,6 +588,7 @@ function feasible_sdp_conic(T)
     ed = 2
     q = [2,3,4]
     s = [2,3,4]
+    cs = Int[]
 
     b = [
     -0.757244022903780567,  0.000000000000000000, -1.892151461056349238,
@@ -642,7 +645,34 @@ function feasible_sdp_conic(T)
     -0.444627816446985402,  0.443421912904091331,  0.182452167505983420]
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, Float64[])
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, Float64[])
+    @test sol.ret_val == 1
+    @test SCS.raw_status(sol.info) == "solved"
+end
+
+# Problem data taken from https://github.com/cvxgrp/scs/blob/master/test/problems/complex_PSD.h
+function feasible_complex_sdp(T)
+    n = 9
+    m = 10
+    z = 1
+    bu= Float64[]
+    bl= Float64[]
+    l = 0
+    ep = 0
+    ed = 0
+    p = Float64[]
+    q = Int[]
+    s = Int[]
+    cs = [3]
+
+    b = [1.0, 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+    c = [1, 2√2, 3√2, 4√2, 5√2, 6, 7√2, -8√2, 9]
+    rowval = [0, 1, 2, 3, 4, 5, 0, 6, 7, 8, 0, 9]
+    colptr = [0, 2, 3, 4, 5, 6, 8, 9, 10, 12]
+    values = [1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, -1.0, 1.0, -1.0]
+    A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
+    P = spzeros(n, n)
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, p)
     @test sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
 end
@@ -659,6 +689,7 @@ function feasible_pow_conic(T)
     ed = 0
     q = Int[]
     s = Int[]
+    cs = Int[]
     p = [-3.0e-01, 2.5e-01, 7.5e-01, -4.0e-01, 8.7e-01, -1.2e-01]
     b = [
     -0.887386390749058451,  1.868842541924223610, -0.755288651776904296,
@@ -689,7 +720,7 @@ function feasible_pow_conic(T)
      0.147891351014747152]
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, p)
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, p)
     @test sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
     return
@@ -712,6 +743,7 @@ function feasible_basic_problems(solver)
         Float64[],
         Int[],
         Int[],
+        Int[],
         0,
         0,
         Float64[],
@@ -720,6 +752,7 @@ function feasible_basic_problems(solver)
     feasible_basic_conic(solver)
     feasible_exponential_conic(solver)
     feasible_sdp_conic(solver)
+    feasible_complex_sdp(solver)
     feasible_pow_conic(solver)
     return
 end
@@ -749,6 +782,7 @@ function test_options(T)
         bl = Float64[],
         q = Int64[],
         s = Int64[],
+        cs = Int64[],
         ep = 0,
         ed = 0,
         p = Float64[],
@@ -805,6 +839,7 @@ function test_scs_solve_solution_vectors(solver)
         Float64[],  # bl
         Int[],  # q
         Int[],  # s
+        Int[],  # cs
         0,  # ep
         0,  # ed
         Float64[],  # p
@@ -827,6 +862,7 @@ function test_scs_solve_solution_vectors(solver)
         Float64[],  # bl
         Int[],  # q
         Int[],  # s
+        Int[],  # cs
         0,  # ep
         0,  # ed
         Float64[],  # p
@@ -851,6 +887,7 @@ function test_scs_solve_solution_vectors(solver)
             Float64[],  # bl
             Int[],  # q
             Int[],  # s
+            Int[],  # cs
             0,  # ep
             0,  # ed
             Float64[],  # p

Project.toml
@@ -21,9 +21,9 @@ SCSSCS_MKL_jllExt = ["SCS_MKL_jll"]
 MathOptInterface = "1.20"
 Pkg = "<0.0.1, ^1.6"
 PrecompileTools = "1"
-SCS_GPU_jll = "=3.2.4, =3.2.6, =3.2.7"
-SCS_MKL_jll = "=3.2.4, =3.2.6, =3.2.7"
-SCS_jll = "=3.2.4, =3.2.6, =3.2.7"
+SCS_GPU_jll = "=3.2.8"
+SCS_MKL_jll = "=3.2.8"
+SCS_jll = "=3.2.8"
 SparseArrays = "<0.0.1, ^1.6"
 Test = "<0.0.1, ^1.6"
 julia = "1.10"

src/MOI_wrapper/MOI_wrapper.jl
@@ -390,6 +390,7 @@ function MOI.optimize!(
         Float64[], # # placeholder: bl
         _map_sets(MOI.dimension, T, Ab, MOI.SecondOrderCone),
         _map_sets(MOI.side_dimension, T, Ab, ScaledPSDCone),
+        Int[], # placeholder complex PSD
         div(Ab.sets.num_rows[5] - Ab.sets.num_rows[4], 3),
         div(Ab.sets.num_rows[6] - Ab.sets.num_rows[5], 3),
         vcat(

commit ea3d24c2ec5ceda9061d576e7fca69fc51d72393

Fix breaking change to scs_solve that was introduced by #311

src/c_wrapper.jl
@@ -409,3 +409,59 @@ function _unsafe_scs_solve(model::_ScsDataWrapper{S,T}) where {S,T}
     Base.Libc.flush_cstdio()
     return Solution(model.primal, model.dual, model.slack, scs_info, status)
 end
+
+# The following is a backward compatible method that does not have the `cs`
+# argument that was introduced in SCS.jl v2.2.0. The underlying C API had a
+# breaking change, but we don't need to expose it to the users of SCS.jl.
+#
+# It is important the function signature of this method matches the function
+# signature of the full `scs_solve` exactly to avoid ambiguity errors.
+function scs_solve(
+    linear_solver::Type{<:LinearSolver},
+    m::Integer,
+    n::Integer,
+    A,
+    P,
+    b::Vector{Float64},
+    c::Vector{Float64},
+    z::Integer,
+    l::Integer,
+    bu::Vector{Float64},
+    bl::Vector{Float64},
+    q::Vector{<:Integer},
+    s::Vector{<:Integer},
+    # cs::Vector{<:Integer},  # Skip this argument
+    ep::Integer,
+    ed::Integer,
+    p::Vector{Float64},
+    primal_sol::Vector{Float64} = zeros(n),
+    dual_sol::Vector{Float64} = zeros(m),
+    slack::Vector{Float64} = zeros(m);
+    warm_start::Bool = false,
+    options...,
+)
+    return scs_solve(
+        linear_solver,
+        m,
+        n,
+        A,
+        P,
+        b,
+        c,
+        z,
+        l,
+        bu,
+        bl,
+        q,
+        s,
+        # cs,  # Skip this argument
+        ep,
+        ed,
+        p,
+        primal_sol,
+        dual_sol,
+        slack;
+        warm_start,
+        options...,
+    )
+end

test/test_problems.jl
@@ -22,7 +22,6 @@ function feasible_basic_conic(T)
     ed = 0
     q = [12]
     s = Int[]
-    cs = Int[]
 
     b = [
     1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
@@ -441,7 +440,7 @@ function feasible_basic_conic(T)
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
 
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, Float64[])
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, Float64[])
     @test sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
 end
@@ -459,7 +458,6 @@ function feasible_exponential_conic(T)
     ed = 5
     q = [0,1,0,2,3]
     s = Int[]
-    cs = Int[]
 
     b = [
     -0.883995908378185202,  0.247092008779755457,  0.000000000000000000,
@@ -569,7 +567,7 @@ function feasible_exponential_conic(T)
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
 
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, Float64[])
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, Float64[])
     @assert sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
 end
@@ -588,7 +586,6 @@ function feasible_sdp_conic(T)
     ed = 2
     q = [2,3,4]
     s = [2,3,4]
-    cs = Int[]
 
     b = [
     -0.757244022903780567,  0.000000000000000000, -1.892151461056349238,
@@ -645,7 +642,7 @@ function feasible_sdp_conic(T)
     -0.444627816446985402,  0.443421912904091331,  0.182452167505983420]
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, Float64[])
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, Float64[])
     @test sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
 end
@@ -689,7 +686,6 @@ function feasible_pow_conic(T)
     ed = 0
     q = Int[]
     s = Int[]
-    cs = Int[]
     p = [-3.0e-01, 2.5e-01, 7.5e-01, -4.0e-01, 8.7e-01, -1.2e-01]
     b = [
     -0.887386390749058451,  1.868842541924223610, -0.755288651776904296,
@@ -720,7 +716,7 @@ function feasible_pow_conic(T)
      0.147891351014747152]
     A = SparseMatrixCSC(m, n, colptr .+ 1, rowval .+ 1, vec(values))
     P = spzeros(n, n)
-    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, cs, ep, ed, p)
+    sol = scs_solve(T, m, n, A, P, b, c, z, l, bu, bl, q, s, ep, ed, p)
     @test sol.ret_val == 1
     @test SCS.raw_status(sol.info) == "solved"
     return
@@ -741,7 +737,6 @@ function feasible_basic_problems(solver)
         0,
         Float64[],
         Float64[],
-        Int[],
         Int[],
         Int[],
         0,
@@ -782,7 +777,6 @@ function test_options(T)
         bl = Float64[],
         q = Int64[],
         s = Int64[],
-        cs = Int64[],
         ep = 0,
         ed = 0,
         p = Float64[],
@@ -839,7 +833,6 @@ function test_scs_solve_solution_vectors(solver)
         Float64[],  # bl
         Int[],  # q
         Int[],  # s
-        Int[],  # cs
         0,  # ep
         0,  # ed
         Float64[],  # p
@@ -862,7 +855,6 @@ function test_scs_solve_solution_vectors(solver)
         Float64[],  # bl
         Int[],  # q
         Int[],  # s
-        Int[],  # cs
         0,  # ep
         0,  # ed
         Float64[],  # p
@@ -887,7 +879,6 @@ function test_scs_solve_solution_vectors(solver)
             Float64[],  # bl
             Int[],  # q
             Int[],  # s
-            Int[],  # cs
             0,  # ep
             0,  # ed
             Float64[],  # p