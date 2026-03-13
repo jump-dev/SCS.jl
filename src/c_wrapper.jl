303a3711049c0257381342695011657d84eef518
Mateus Araújo <maltusan@gmail.com>
2026-03-13 16:37:06 +0100

    no longer needed

--- src/c_wrapper.jl
+++ src/c_wrapper.jl
@@ -428,14 +428,9 @@ function _unsafe_scs_solve(model::_ScsDataWrapper{S,T}) where {S,T}
         pointer(model.c),
     )
     for (key, value) in model.options
-        if value isa AbstractString
-            if value isa String
-                cstr =
-                    Base.unsafe_convert(Cstring, Base.cconvert(Cstring, value))
-                setproperty!(model.settings, key, cstr)
-            else
-                error("You must pass a `String` as the value for $(key)")
-            end
+        if value isa String
+            cstr = Base.unsafe_convert(Cstring, Base.cconvert(Cstring, value))
+            setproperty!(model.settings, key, cstr)
         else
             setproperty!(model.settings, key, value)
         end
